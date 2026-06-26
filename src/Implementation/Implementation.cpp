91935450f80c8aab721d4ba02075cf987e005060
Catalin Chirosca <catalin270402@gmail.com>
2026-06-26 20:24:43 +0200

    Added Implementation

---
+++ src/Implementation/Implementation.cpp
@@ -0,0 +1,20 @@
+//
+// Module: Implementation
+// File: Implementation.cpp
+// Created by: Catalin Chirosca
+// Created: 2026-06-26
+// Updated by: Catalin Chirosca
+// Updated: 2026-06-26
+//
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTLFX_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+
+#include "Foundation/Foundation.hpp"
+#include "Metal/Metal.hpp"
+#include "MetalFX/MetalFX.hpp"
+#include "MetalKit/MetalKit.hpp"
+#include "QuartzCore/QuartzCore.hpp"
